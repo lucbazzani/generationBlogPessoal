94cebea6a13e5055fe571c1a0b681a8d2cbe68ae
lucbazzani <lu.bazzani@hotmail.com>
2021-08-05 13:58:33 -0300

    BasicSecurityConfig

---
+++ src/main/java/org/generation/blogPessoal/seguranca/BasicSecurityConfig.java
@@ -0,0 +1,42 @@
+package org.generation.blogPessoal.seguranca;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Override  //metódo configure pertence ao userDetailsService
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService);
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers("/usuarios/logar").permitAll() //liberar endpoint para o controller não passar token
+		.antMatchers("/usuarios/cadastrar").permitAll()
+		.anyRequest().authenticated()  //todas as outras requisições deverão passar a chava/token
+		.and().httpBasic() //gerar chave/token
+		.and().sessionManagement() //indica o tipo de sessão que será utilizada
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS) //a sessão criada será STATELESS (não será guardada)
+		.and().cors()
+		.and().csrf().disable();
+	}
+}
